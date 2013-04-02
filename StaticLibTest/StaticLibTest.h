9ee3b1a4ee1cae2a0c12e25f6b1fda3d96e0ae14
Josh Sloat <jsloat@articulate.com>
2013-04-02 08:28:55 -0400

    Initial Commit

---
+++ StaticLibTest/StaticLibTest.h
@@ -0,0 +1,13 @@
+//
+//  StaticLibTest.h
+//  StaticLibTest
+//
+//  Created by Josh Sloat on 4/2/13.
+//  Copyright (c) 2013 Josh Sloat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibTest : NSObject
+
+@end
---
+++ StaticLibTest/StaticLibTest.m
@@ -0,0 +1,13 @@
+//
+//  StaticLibTest.m
+//  StaticLibTest
+//
+//  Created by Josh Sloat on 4/2/13.
+//  Copyright (c) 2013 Josh Sloat. All rights reserved.
+//
+
+#import "StaticLibTest.h"
+
+@implementation StaticLibTest
+
+@end
